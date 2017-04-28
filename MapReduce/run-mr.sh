93468340cbcbcb0af10ab4c2b334eef2d81ab911
yongaaaaaa <yongaaaaaa@naver.com>
2017-04-29 03:46:22 +0900

    final check 2017.04.30

--- MapReduce/run-mr.sh
+++ MapReduce/run-mr.sh
@@ -1,5 +1,5 @@
 hadoop fs -rm -r s3://<bucket name>/<output path>
 hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
-           -input s3://<bucket name>/<input path> -output /<bucket name>/<output path>  \
+           -input s3://<bucket name>/<input path> -output s3://<bucket name>/<output path>  \
            -file /<local path>/mapper.py -mapper /<local path>/mapper.py \
            -file /<local path>/reducer.py -reducer /<local path>/reducer.py
